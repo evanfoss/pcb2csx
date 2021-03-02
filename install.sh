46593e8ee8fb58a5619e798a2bd93235c12c487b
evanfoss <evanfoss@gmail.com>
2021-03-02 06:16:21 -0500

    add: less needless files copied

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 mkdir ~/programs
 mkdir ~/programs/pcb2csx
-cp -R * ~/programs/pcb2csx/
+cp -R matlab ~/programs/pcb2csx/
+cp -R examples ~/programs/pcb2csx/
+cp LICENSE ~/programs/pcb2csx/
 echo "addpath('~/programs/pcb2csx/matlab');" >> ~/.octaverc
 
 
